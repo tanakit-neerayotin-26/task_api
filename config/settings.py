0318f459d0b8da6364f0109cb062e17ff30944d0
tanakit-neerayotin-26 <tanakitguy26@gmail.com>
2026-09-03 21:13:52 +0700

    Configure allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG','True') == True
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = [
+    "taskapi-production-f5ca.up.railway.app",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
