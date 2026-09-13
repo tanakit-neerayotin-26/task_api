19a7b102ae993832d285e1a0a4dc21701cc1f96f
tanakit-neerayotin-26 <tanakitguy26@gmail.com>
2026-09-13 16:38:04 +0700

    Update API configuration

--- task/serializer.py
+++ task/serializer.py
@@ -7,6 +7,18 @@ class TaskSerializer(serializers.ModelSerializer):
         model = Task
         fields = ['id','title', 'description', 'status', 'priority', 'created_at', 'updated_at']
 
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+     model = User
+     fields= ['username', 'password']
+
+    def create(self,validated_data):
+        user = User.objects.create_user(
+        username=validated_data['username'],
+        password=validated_data['password']
+             )
+        return user
+
 
 
 
--- task/urls.py
+++ task/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet, health_check
+from .views import TaskViewSet, health_check, RegisterView
 from rest_framework.authtoken.views import obtain_auth_token
+
 
 router = DefaultRouter()
 router.register(r'task', TaskViewSet, basename='task')
@@ -9,5 +10,6 @@ router.register(r'task', TaskViewSet, basename='task')
 urlpatterns = [
     
     path('', include(router.urls)),
-    path('token/', obtain_auth_token, name='api_token_auth')
+    path('token/', obtain_auth_token, name='api_token_auth'),
+    path('register/', RegisterView.as_view(), name='register')
 ]
